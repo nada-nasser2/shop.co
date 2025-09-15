ffcee1f4f65b0cbac2a74df4252b5461cad32bd1
nada-nasser2 <nadanassereissa@gmail.com>
2025-09-15 22:20:59 +0300

    update2

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const sideBar = document.querySelector(".sideBar");
+const listDropDown =document.querySelector('.list-down');
+const navBar = document.querySelector(".navBar");
+
 document.querySelector(".iconBars").addEventListener("click", function () {
   if (sideBar.style.display === "block") {
     sideBar.style.display = "none";
@@ -6,3 +9,16 @@ document.querySelector(".iconBars").addEventListener("click", function () {
     sideBar.style.display = "block";
   }
 });
+
+
+listDropDown.addEventListener( 'click' , ()=>{
+  listDropDown.classList.toggle('active')
+})
+
+
+function setTop() {
+  sideBar.style.top = navBar.offsetHeight + "px";
+}
+
+window.addEventListener("resize", setTop);
+setTop();
